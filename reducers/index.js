7650e2bbdaa48491a683a1ff8c1dd13b9bc26911
Geovane-Oliveira <speedtestf1@gmail.com>
2020-05-15 00:28:48 -0300

    Inicio Firestore sem name

---
+++ reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux'
+import { LOGIN, SIGNUP, UPDATE_NAME, UPDATE_EMAIL, UPDATE_PASSWORD } from '../actions/user'
+
+const user = (state = {}, action) => {
+	switch (action.type) {
+		case LOGIN:
+			return action.payload
+		case SIGNUP:
+			return action.payload
+		case UPDATE_NAME:
+			return { ...state, displayName: action.payload }
+		case UPDATE_EMAIL:
+			return { ...state, email: action.payload }
+		case UPDATE_PASSWORD:
+			return { ...state, password: action.payload }
+		default:
+			return state
+	}
+}
+
+const rootReducer = combineReducers({
+	user
+})
+
+export default rootReducer
---
+++ screens/Signup.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import { View, TextInput, StyleSheet, TouchableOpacity, Text } from 'react-native'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { updateName, updateEmail, updatePassword, signup } from '../actions/user'
+
+class Signup extends React.Component {
+	handleSignUp = () => {
+		this.props.signup()
+		this.props.navigation.navigate('Profile')
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<TextInput
+					style={styles.inputBox}
+					value={this.props.user.name}
+					onChangeText={name => this.props.updateName(name)}
+					placeholder='Nome'
+					autoCapitalize='none'
+				/>
+				<TextInput
+					style={styles.inputBox}
+					value={this.props.user.email}
+					onChangeText={email => this.props.updateEmail(email)}
+					placeholder='Email'
+					autoCapitalize='none'
+				/>
+				<TextInput
+					style={styles.inputBox}
+					value={this.props.user.password}
+					onChangeText={password => this.props.updatePassword(password)}
+					placeholder='Senha'
+					secureTextEntry={true}
+				/>
+				<TouchableOpacity style={styles.button} onPress={this.handleSignUp}>
+					<Text style={styles.buttonText}>Cadastrar</Text>
+				</TouchableOpacity>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	inputBox: {
+		width: '85%',
+		margin: 10,
+		padding: 15,
+		fontSize: 16,
+		borderColor: '#d3d3d3',
+		borderBottomWidth: 1,
+		textAlign: 'center'
+	},
+	button: {
+		marginTop: 30,
+		marginBottom: 20,
+		paddingVertical: 5,
+		alignItems: 'center',
+		backgroundColor: '#FFA611',
+		borderColor: '#FFA611',
+		borderWidth: 1,
+		borderRadius: 5,
+		width: 200
+	},
+	buttonText: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		color: '#fff'
+	},
+	buttonSignup: {
+		fontSize: 12
+	}
+})
+
+const mapDispatchToProps = dispatch => {
+	return bindActionCreators({ updateName, updateEmail, updatePassword, signup }, dispatch)
+}
+
+const mapStateToProps = state => {
+	return {
+		user: state.user
+	}
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Signup)
